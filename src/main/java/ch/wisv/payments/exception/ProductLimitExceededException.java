0cb3c5515da1eb5985c62f90175ba62a36fce556
Sille Kamoen <sille.kamoen@gmail.com>
2017-03-14 13:59:08 +0100

    Set status codes for exceptions

--- src/main/java/ch/wisv/payments/exception/ProductLimitExceededException.java
+++ src/main/java/ch/wisv/payments/exception/ProductLimitExceededException.java
@@ -1,5 +1,9 @@
 package ch.wisv.payments.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class ProductLimitExceededException extends RuntimeException {
     public ProductLimitExceededException(String s) {
         super(s);
