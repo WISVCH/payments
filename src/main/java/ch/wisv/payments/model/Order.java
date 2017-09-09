b09e96faa51d9ede1fd4618a10dff9ac1a85c999
Sille Kamoen <sille.kamoen@gmail.com>
2017-09-09 12:40:43 +0200

    Fix test property and change table name for Orders

--- src/main/java/ch/wisv/payments/model/Order.java
+++ src/main/java/ch/wisv/payments/model/Order.java
@@ -4,15 +4,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToMany;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
+@Table(name="orders")
 @NoArgsConstructor
 public class Order {
 
